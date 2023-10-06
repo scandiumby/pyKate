5035fb7cfb50f522336ca85ff3710adf5f068bc1
Nikolay R <ras.nikolay@mail.ru>
2023-10-06 17:13:28 +0300

    Minimal sample firebase routing (v0.0.7)

---
+++ pykate/admin/frontend/src/components/EmailPasswordLogin.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { auth } from './firebase'; // Firebase auth module
+
+function EmailPasswordLogin() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            // Successful login
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default EmailPasswordLogin;
